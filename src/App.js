1db9ac9a865be69530b800385aa6e83c346e168e
Chris hsiao <chris.hsiao@igroup.tw>
2020-06-18 23:19:59 +0800

    [新增] 基礎使用範例

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+} from 'react-router-dom';
 import Home from './View/Home';
 
 class App extends Component {
@@ -11,10 +17,48 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <Home />
+                <Router>
+                    <div>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/about">About</Link>
+                                </li>
+                                <li>
+                                    <Link to="/users">Users</Link>
+                                </li>
+                            </ul>
+                        </nav>
+
+                        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                        <Switch>
+                            <Route path="/about">
+                                <About />
+                            </Route>
+                            <Route path="/users">
+                                <Users />
+                            </Route>
+                            <Route path="/">
+                                <Home />
+                            </Route>
+                        </Switch>
+                    </div>
+                </Router>
             </div>
         );
     }
 }
 
 export default hot(module)(App);
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Users() {
+    return <h2>Users</h2>;
+}
